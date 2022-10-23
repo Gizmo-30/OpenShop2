c184e00b46ae932e37520f7b90f617b50a93db9d
gizmo <anvaradizov80@gmail.com>
2022-10-23 12:30:09 +0500

    third

--- script.js
+++ script.js
@@ -41,12 +41,32 @@ function FuncOpen() {
     prenavContent.style = `justify-content: end;`;
     return;
 }
-var mobile 
 
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    mobile = true
+
+let isMobile = {
+    Android: function () { return navigator.userAgent.match(/Android/i); },
+    BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+    iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+    Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+    Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+    any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+};
+
+let body = document.querySelector('body'),
+    arrow = document.querySelectorAll('.arrow');
+
+if (isMobile.any()) {
+    body.classList.add('touch')
+    for (i = 0; i < arrow.length; i++) {
+        let subMenu = arrow[i].nextElementSibling,
+            thisLink = arrow[i].previousElementSibling,
+            thisArrow = arrow[i];
+        thisLink.classList.add('active')
+        arrow[i].addEventListener('click', function () {
+            subMenu.classList.toggle('open')
+            thisArrow.classList.toggle('active')
+        })
+    }
 } else {
-    mobile = false
+    body.classList.add('mouse')
 }
-
-console.log(mobile);
